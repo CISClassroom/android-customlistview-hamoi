ef465817c21db43e3b929d91f46112a0700f5f6c
wtt-kku <withan@kkumail.com>
2020-01-10 00:43:27 +0700

    AddProject

---
+++ app/src/main/java/com/example/customlistviews/DataActivity.kt
@@ -0,0 +1,53 @@
+package com.example.customlistviews
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class DataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data)
+
+        //รับข้อมูลจาก Intent
+
+        var intent = intent
+        val get_fullname = intent.getStringExtra("fn")
+        val get_idstudent = intent.getStringExtra("id")
+        val get_tel = intent.getStringExtra("tel")
+        val get_email = intent.getStringExtra("email")
+        val get_git = intent.getStringExtra("git")
+
+
+
+        val fullname = findViewById<TextView>(R.id.display_fullname)
+        val idstudent = findViewById<TextView>(R.id.display_id)
+        val major = findViewById<TextView>(R.id.display_major)
+        val telenumber = findViewById<TextView>(R.id.display_tel)
+        val email = findViewById<TextView>(R.id.display_email)
+        val github = findViewById<TextView>(R.id.display_github)
+        val image = findViewById<ImageView>(R.id.display_img)
+
+
+
+        fullname.text = get_fullname
+        idstudent.text = "รหัสนักศึกษา "+get_idstudent
+        major.text = "สาขาวิทยาการคอมพิวเตอร์และสารสนเทศ"
+        telenumber.text = get_tel
+        email.text = get_email
+        github.text = get_git
+
+        when (get_idstudent) {
+            "603410061-2" -> image.setImageResource(R.drawable.tor)
+            else -> image.setImageResource(R.mipmap.ic_launcher)
+        }
+
+
+
+        //startActivity(intent)
+
+    }
+}
---
+++ app/src/main/java/com/example/customlistviews/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.customlistviews
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.os.bundleOf
+import kotlinx.android.synthetic.main.row.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var listView = findViewById<ListView>(R.id.listView)
+
+        var list = mutableListOf<Model>()
+        listView.adapter = MyAdapter(this,R.layout.row,list)
+        list.add(Model("วิธาน   วงษาบุตร","603410061-2",R.drawable.tor,"089-818-2239","withan@kkumail.com","https://github.com/wtt-kku"))
+
+        listView.setOnItemClickListener { parent:AdapterView<*>, view:View, position:Int, id:Long ->
+            if (position == 0)
+            {
+                val fullname = list[0].fullname
+                val idstudent = list[0].idstudent
+                val tele = list[0].tel
+                val email = list[0].email
+                val github = list[0].github
+                val intent = Intent(this@MainActivity, DataActivity::class.java)
+                intent.putExtra("fn",fullname)
+                intent.putExtra("id",idstudent)
+                intent.putExtra("tel",tele)
+                intent.putExtra("email",email)
+                intent.putExtra("git",github)
+                startActivity(intent)
+                //Toast.makeText(this@MainActivity,"BOSS",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
+    }
+}
+
+
